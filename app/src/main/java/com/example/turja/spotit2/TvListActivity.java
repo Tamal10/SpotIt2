2753a2cd0dc500272c3102d7b0b3241412210514
STamal <shareef.tamal@gmail.com>
2017-01-16 17:24:30 +0600

    lists in table

--- app/src/main/java/com/example/turja/spotit2/TvListActivity.java
+++ app/src/main/java/com/example/turja/spotit2/TvListActivity.java
@@ -1,12 +1,17 @@
 package com.example.turja.spotit2;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -43,34 +48,128 @@ public class TvListActivity extends Activity {
                     R.layout.search_item, bundle.getStringArrayList("dateTime"));
             description = new ArrayAdapter<String>(this,
                     R.layout.search_item, bundle.getStringArrayList("description"));
-            ListView lv1 = (ListView) findViewById(R.id.loc);
-            lv1.setAdapter(location);
-            ListView lv2 = (ListView) findViewById(R.id.typ);
-            lv2.setAdapter(type);
-            final ListView lv3 = (ListView) findViewById(R.id.tim);
-            lv3.setAdapter(dayTime);
+//            ListView lv1 = (ListView) findViewById(R.id.loc);
+//            lv1.setAdapter(location);
+//            ListView lv2 = (ListView) findViewById(R.id.typ);
+//            lv2.setAdapter(type);
+//            final ListView lv3 = (ListView) findViewById(R.id.tim);
+//            lv3.setAdapter(dayTime);
+//
+//            lv2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                public void onItemClick(AdapterView<?> parent, View view,
+//                                        int position, long id) {
+////                    String s= (String) lv3.getItemAtPosition(position);
+//                    ArrayList<String> toPass=new ArrayList<String>();
+//
+//
+//
+//                    toPass.add(Integer.toString(ids.get(position)));
+//                    toPass.add((String) type.getItem(position));
+//                    toPass.add((String) location.getItem(position));
+//                    toPass.add((String) dayTime.getItem(position));
+//                    toPass.add((String) description.getItem(position));
+//
+//                    new SendPhotoRequest(view.getContext()).execute(toPass);
+//
+////                    Intent intent=new Intent(view.getContext(),ViolationDetailsActivity.class);
+////                    intent.putExtra("values",toPass);
+////                    startActivity(intent);
+//                }
+//            });
 
-            lv2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                public void onItemClick(AdapterView<?> parent, View view,
-                                        int position, long id) {
-//                    String s= (String) lv3.getItemAtPosition(position);
-                    ArrayList<String> toPass=new ArrayList<String>();
+            TableLayout t1 = null;
+
+            TableLayout tl = (TableLayout) findViewById(R.id.tv_list);
+            TableRow thead=new TableRow(this);
+            thead.setPadding(5,0,0,5);
+
+            TextView col1 = new TextView(this);
+            col1.setId(View.generateViewId());
+            col1.setText("Date Time");
+            col1.setTextSize(18);
+            col1.setPadding(10,10,10,10);
+
+            col1.setTextColor(Color.BLUE);
+            thead.addView(col1);
+
+            TextView col2 = new TextView(this);
+            col2.setId(View.generateViewId());
+            col2.setText("Location");
+            col2.setTextSize(18);
+            col2.setPadding(15,10,10,10);
+            col2.setTextColor(Color.BLUE);
+            thead.addView(col2);
+
+            TextView col3 = new TextView(this);
+            col3.setId(View.generateViewId());
+            col3.setText("Type");
+            col3.setTextSize(18);
+            col3.setPadding(15,10,10,10);
+            col3.setTextColor(Color.BLUE);
+            thead.addView(col3);
+
+            tl.addView(thead, new TableLayout.LayoutParams(
+                    LinearLayoutCompat.LayoutParams.FILL_PARENT,
+                    LinearLayoutCompat.LayoutParams.WRAP_CONTENT));
 
 
-
-                    toPass.add(Integer.toString(ids.get(position)));
-                    toPass.add((String) type.getItem(position));
-                    toPass.add((String) location.getItem(position));
-                    toPass.add((String) dayTime.getItem(position));
-                    toPass.add((String) description.getItem(position));
-
-                    new SendPhotoRequest(view.getContext()).execute(toPass);
-
-//                    Intent intent=new Intent(view.getContext(),ViolationDetailsActivity.class);
-//                    intent.putExtra("values",toPass);
-//                    startActivity(intent);
+            for(int i=0;i<ids.size();i++) {
+                TableRow tr_head = new TableRow(this);
+                tr_head.setPadding(5, 0, 0, 5);
+                tr_head.setTag(i);
+                tr_head.setFocusable(true);
+                tr_head.setClickable(true);
+                if(i%2==0){
+                    tr_head.setBackgroundColor(Color.GRAY);
                 }
-            });
+                TextView date = new TextView(this);
+                date.setId(View.generateViewId());
+                date.setText(dayTime.getItem(i).toString());
+                date.setTextSize(18);
+                date.setPadding(10,10,10,10);
+                date.setTextColor(Color.BLACK);
+                tr_head.addView(date);
+
+                TextView loc = new TextView(this);
+                loc.setId(View.generateViewId());
+                loc.setText(location.getItem(i).toString());
+                loc.setTextSize(18);
+                loc.setPadding(15,10,10,10);
+                loc.setTextColor(Color.BLACK);
+                tr_head.addView(loc);
+
+                TextView typ = new TextView(this);
+                typ.setId(View.generateViewId());
+                typ.setText(type.getItem(i).toString());
+                typ.setTextSize(18);
+                typ.setPadding(15,10,10,10);
+                typ.setTextColor(Color.BLACK);
+                tr_head.addView(typ);
+
+                tl.addView(tr_head, new TableLayout.LayoutParams(
+                        LinearLayoutCompat.LayoutParams.FILL_PARENT,
+                        LinearLayoutCompat.LayoutParams.WRAP_CONTENT));
+
+                tr_head.setOnClickListener(new View.OnClickListener() {
+                       @Override
+                       public void onClick(View view) {
+                            int position= (int) view.getTag();
+//                           Toast toast = Toast.makeText(getApplicationContext(), "Tabele row clicked "+ tag , Toast.LENGTH_LONG);
+//                           toast.show();
+                            ArrayList<String> toPass=new ArrayList<String>();
+
+                            toPass.add(Integer.toString(ids.get(position)));
+                            toPass.add((String) type.getItem(position));
+                            toPass.add((String) location.getItem(position));
+                            toPass.add((String) dayTime.getItem(position));
+                            toPass.add((String) description.getItem(position));
+
+                            new SendPhotoRequest(view.getContext()).execute(toPass);
+                       }
+                   }
+
+                );
+            }
         }
 
     }
--- app/src/main/java/com/example/turja/spotit2/ViolationDetailsActivity.java
+++ app/src/main/java/com/example/turja/spotit2/ViolationDetailsActivity.java
@@ -3,11 +3,17 @@ package com.example.turja.spotit2;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Base64;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class ViolationDetailsActivity extends Activity {
     String []key={"Violation Type","Location","Time","Details"};
@@ -23,16 +29,53 @@ public class ViolationDetailsActivity extends Activity {
         value[2]=bundle.getStringArrayList("values").get(3);
         value[3]=bundle.getStringArrayList("values").get(4);
         String photo=bundle.getStringArrayList("values").get(5);
-        ListView lv1= (ListView) findViewById(R.id.key);
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,
-                R.layout.search_item, key);
-        lv1.setAdapter(adapter);
-        ListView lv2= (ListView) findViewById(R.id.value);
+//        ListView lv1= (ListView) findViewById(R.id.key);
+//        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+//                R.layout.search_item, key);
+//        lv1.setAdapter(adapter);
+//        ListView lv2= (ListView) findViewById(R.id.value);
+//
+//        ArrayAdapter adapter1 = new ArrayAdapter<String>(this,
+//                R.layout.search_item, value);
+//        lv2.setAdapter(adapter1);
+        TableLayout t1 = null;
 
-        ArrayAdapter adapter1 = new ArrayAdapter<String>(this,
-                R.layout.search_item, value);
-        lv2.setAdapter(adapter1);
+        TableLayout tl = (TableLayout) findViewById(R.id.vio_details);
 
+        for(int i=0;i<key.length;i++) {
+            TableRow tr_head = new TableRow(this);
+            tr_head.setPadding(5,0,0,5);
+
+            if(i%2==0){
+                tr_head.setBackgroundColor(Color.GRAY);
+            }
+
+            TextView tkey = new TextView(this);
+            tkey.setId(View.generateViewId());
+            tkey.setText(key[i]);
+            tkey.setTextColor(Color.BLUE);
+            tkey.setTextSize(24);
+
+
+
+            tkey.setPadding(15, 15, 5, 5);
+
+            tr_head.addView(tkey);
+
+            TextView tval = new TextView(this);
+            tval.setId(View.generateViewId());
+            tval.setText(value[i]);
+            tval.setTextSize(24);
+            tval.setTextColor(Color.BLACK);
+            tval.setPadding(18, 15, 5, 5);
+            tr_head.addView(tval);
+
+
+            tl.addView(tr_head, new TableLayout.LayoutParams(
+                    LinearLayoutCompat.LayoutParams.FILL_PARENT,
+                    LinearLayoutCompat.LayoutParams.WRAP_CONTENT));
+        }
+//        t1.setPadding(15,15,15,15);
         ImageView iv= (ImageView) findViewById(R.id.imageView);
 
         byte[] decodedString = Base64.decode(photo, Base64.DEFAULT);
