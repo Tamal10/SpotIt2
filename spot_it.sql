55ed4046621128777c0107b4c20221c5bbfd33b9
STamal <shareef.tamal@gmail.com>
2017-01-20 14:15:36 +0600

    sql file

--- spot_it.sql
+++ spot_it.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 17, 2017 at 12:22 PM
+-- Generation Time: Jan 20, 2017 at 09:15 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -41,46 +41,37 @@ CREATE TABLE IF NOT EXISTS `traffic_violation` (
 --
 
 INSERT INTO `traffic_violation` (`id`, `location`, `date_time`, `day`, `photo_id`, `type`, `description`) VALUES
-(1, 'farmgate', '2017-01-07 07:09:39', '', 1, 'signal break', NULL),
-(2, 'mirpur', '2017-01-04 14:30:00', '', 2, 'motorcycle in footpath', 'foul motorbikewala'),
-(7, 'md. pur', '2017-01-03 01:45:00', '', 5, 'vugichugi', 'another vugichugiuu'),
-(8, 'md. pur', '2017-01-03 01:45:00', '', 5, 'vugichugi', 'another vugichugiuu'),
-(9, 'ffa', '2017-01-04 15:08:00', '', 4, 'Signal Break', 'fasfa'),
-(10, 'hfhdhh', '2016-12-31 15:17:00', '', 4, 'Signal Break', 'lllllll'),
-(11, 'gdgsdgsd', '2017-01-02 12:06:00', '', 4, 'Signal Break', 'dagfagasga'),
-(12, 'fffsa', '2017-01-10 13:24:00', '', 4, 'Signal Break', 'fsaffsa'),
-(13, 'fsfaf', '2017-01-03 12:32:00', '', 4, 'Signal Break', 'fsagrhshtht'),
-(14, 'Polashi', '2017-01-06 17:25:00', 'null', 4, 'Signal Break', 'gggg'),
-(15, 'Polashi', '2017-01-06 14:36:00', 'Friday', 0, 'Signal Break', 'llll'),
-(16, 'Polashi', '2017-01-05 15:43:00', 'Thursday', 0, 'Signal Break', 'sadasdad'),
-(17, 'Polashi', '2017-01-11 15:43:00', 'Wednesday', 0, 'Signal Break', 'sadasdad'),
-(18, 'Polashi', '2017-01-07 15:43:00', 'Saturday', 0, 'Signal Break', 'sadasdad'),
-(19, 'Polashi', '2017-01-08 15:43:00', 'Sunday', 0, 'Signal Break', 'sadasdad'),
-(20, 'Polashi', '2017-01-08 15:43:00', 'Sunday', 0, 'Signal Break', 'sadasdad'),
-(21, 'Polashi', '2017-01-08 15:43:00', 'Sunday', 0, 'Signal Break', 'sadasdad'),
-(22, 'Polashi', '2017-01-07 15:43:00', 'Saturday', 0, 'Signal Break', 'sadasdad'),
-(23, 'Polashi', '2017-01-07 15:43:00', 'Friday', 0, 'Signal Break', 'sadasdad'),
-(24, 'Polashi', '2017-01-11 15:43:00', 'Tuesday', 0, 'Signal Break', 'sadasdad'),
-(25, 'Polashi', '2017-01-02 17:51:00', 'Sunday', 0, 'Signal Break', 'sadasdad'),
-(26, 'Polashi', '2017-01-03 17:51:00', 'Monday', 0, 'Signal Break', 'sadasdad'),
-(27, 'Polashi', '2017-01-03 17:54:00', 'Monday', 0, 'Signal Break', 'sadasdad'),
-(28, 'Polashi', '2017-01-03 17:54:00', 'Tuesday', 0, 'Signal Break', 'sadasdad'),
-(29, 'Polashi', '2017-01-03 17:54:00', 'Tuesday', 0, 'Signal Break', 'sadasdad'),
-(30, 'Polashi', '2017-01-03 17:54:00', 'Tuesday', 0, 'Signal Break', 'sadasdad'),
-(31, 'Polashi', '2017-01-03 17:54:00', 'Tuesday', 0, 'Signal Break', 'sadasdad'),
-(32, 'Polashi', '2017-01-03 17:54:00', 'Tuesday', 0, 'Signal Break', 'sadasdad'),
-(33, 'Polashi', '2017-01-03 15:00:00', 'Tuesday', 0, 'Signal Break', 'sadasdad'),
-(34, 'Polashi', '2017-01-03 15:02:00', 'Tuesday', 0, 'Signal Break', 'sadasdad'),
-(35, 'Polashi', '2017-01-03 03:02:00', 'Tuesday', 0, 'Signal Break', 'sadasdad'),
-(36, 'Polashi', '2017-01-06 03:02:00', 'Friday', 0, 'Signal Break', 'sadasdad'),
-(37, 'g', '2017-01-12 03:24:00', 'Thursday', 0, 'Signal Break', 'g'),
-(38, 'g', '2017-01-12 03:24:00', 'Thursday', 0, 'Signal Break', 'g'),
-(39, 'f', '2017-01-13 06:22:00', 'Friday', 0, 'Signal Break', 'hghf'),
-(40, 'f', '2017-01-13 06:22:00', 'Friday', 0, 'Signal Break', 'hghf'),
-(41, 'f', '2017-01-13 06:22:00', 'Friday', 0, 'Signal Break', 'hghf'),
-(42, 'f', '2017-01-13 06:22:00', 'Friday', 0, 'Signal Break', 'hghf'),
-(43, 'f', '2017-01-13 06:22:00', 'Friday', 0, 'Signal Break', 'hghf'),
-(44, 'f', '2017-01-13 06:22:00', 'Friday', 0, 'Signal Break', 'hghf'),
+(1, 'Farmgate', '2017-01-07 07:09:39', 'Saturday', 1, 'Signal Violation', ' '),
+(2, 'Mirpur 10', '2017-01-04 14:30:00', 'Wednesday', 2, 'Vehicle in Footpath', 'foul motorbikewala'),
+(7, 'Md. pur', '2017-01-03 01:45:00', 'Tuesday', 5, 'Wrong Parking', ' '),
+(8, 'Md. pur', '2017-01-03 01:45:00', 'Tuesday', 5, 'Wrong Lane', 'Secretary''s Car in the wrong lane'),
+(9, 'Mirpur 10', '2017-01-04 15:08:00', 'Wednesday', 4, 'Signal Violation', 'A motorbike with ID gha-123456 violated red signal'),
+(10, 'Mirpur 2', '2016-12-31 15:17:00', 'Saturday', 4, 'Signal Violation', 'Leguna violated signal'),
+(11, 'Badda', '2017-01-02 12:06:00', 'Monday', 4, 'Signal Violation', 'A brown fox run over the lazy dog'),
+(12, 'Rampura', '2017-01-10 13:24:00', 'Tuesday', 4, 'Signal Violation', 'A motorbike with ID gha-123457 violated red signal'),
+(13, 'Farmgate', '2017-01-03 12:32:00', 'Tuesday', 4, 'Wrong Parking', 'A car with ID gha-123456 parked in wrong place'),
+(14, 'Polashi', '2017-01-06 17:25:00', 'Friday', 4, 'Signal Violation', 'A motorbike with ID gha-123458 violated red signal'),
+(15, 'TSC', '2016-12-31 23:36:00', 'Saturday', 0, 'Drunk Driver', 'Drunk student driving motorcycle'),
+(16, 'Dhanmondi 27', '2017-01-05 15:43:00', 'Thursday', 0, 'Signal Violation', 'Dhaka Indira Porbohon Leguna did not obey signal'),
+(17, 'Bangla Motor', '2017-01-11 15:43:00', 'Wednesday', 0, 'Vehicle in Footpath', ' '),
+(18, 'Karwan Bazar', '2017-01-07 15:43:00', 'Saturday', 0, 'Street Crossing', 'A passer by crossed street with risk under foot overbridge'),
+(19, 'Azimpur', '2017-01-08 15:43:00', 'Sunday', 0, 'Signal Violation', 'A bus with ID gha-123456 violated red signal'),
+(20, 'Polashi', '2017-01-08 17:43:00', 'Sunday', 0, 'Wrong Lane', 'MP''s car in wrong lane'),
+(21, 'Motijheel', '2017-01-08 15:43:00', 'Sunday', 0, 'Street Crossing', ' '),
+(22, 'Polashi', '2017-01-07 15:43:00', 'Saturday', 0, 'Signal Violation', 'A rickshaw with ID gha-123456 violated yellow signal'),
+(23, 'Nilkhet', '2017-01-07 15:43:00', 'Friday', 0, 'Signal Violation', 'Leguna violated signal'),
+(24, 'Science Lab', '2017-01-11 15:43:00', 'Tuesday', 0, 'Over Speed', 'Leguna drove in high speed'),
+(25, 'Polashi', '2017-01-02 17:51:00', 'Sunday', 0, 'Signal Violation', 'sadasdad'),
+(26, 'Polashi', '2017-01-03 17:51:00', 'Monday', 0, 'Wrong Parking', 'Bus Parked in wrong place and thus created traffic jam'),
+(28, 'Polashi', '2017-01-03 17:54:00', 'Tuesday', 0, 'Wrong Lane', 'DU''s double decker bus in wrong lane'),
+(29, 'Polashi', '2017-01-17 17:54:00', 'Tuesday', 0, 'Vehicle in Footpath', ' '),
+(30, 'Polashi', '2017-01-10 17:33:00', 'Tuesday', 0, 'Signal Violation', 'A motorbike with ID gha-123456 violated red signal'),
+(31, 'Asad Gate', '2017-01-03 17:54:00', 'Tuesday', 0, 'Wrong Lane', 'Rakin''s car in wrong lane'),
+(32, 'Shantinogor', '2017-01-05 17:54:00', 'Thursday', 0, 'Over Speed', 'Sabbir drove car in high speed'),
+(33, 'Polashi', '2017-01-13 15:00:00', 'Friday', 0, 'Over Speed', 'A motorcycle over speed'),
+(34, 'Polashi', '2017-01-03 15:02:00', 'Tuesday', 0, 'Signal Violation', ' '),
+(35, 'Rampura', '2017-01-03 03:02:00', 'Tuesday', 0, 'Signal Violation', 'Not maintaing signal'),
+(36, 'Polashi', '2017-01-06 03:02:00', 'Friday', 0, 'Street Crossing', ' '),
 (48, 'Farmgate', '2017-01-17 04:39:00', 'Tuesday', 0, 'Drunk Driver', 'Jami Sami');
 
 -- --------------------------------------------------------
@@ -92,7 +83,7 @@ INSERT INTO `traffic_violation` (`id`, `location`, `date_time`, `day`, `photo_id
 CREATE TABLE IF NOT EXISTS `violation_type` (
 `id` int(11) NOT NULL,
   `name` varchar(50) CHARACTER SET utf8 NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `violation_type`
@@ -100,9 +91,13 @@ CREATE TABLE IF NOT EXISTS `violation_type` (
 
 INSERT INTO `violation_type` (`id`, `name`) VALUES
 (1, 'Signal Violation'),
-(2, 'OverSpeed'),
+(2, 'Over Speed'),
 (3, 'Vehicle in Footpath'),
-(4, 'Drunk Driver');
+(4, 'Drunk Driver'),
+(5, 'Wrong Lane'),
+(6, 'Wrong Parking'),
+(7, 'Street Crossing'),
+(8, 'Over Speed');
 
 --
 -- Indexes for dumped tables
@@ -133,7 +128,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=49;
 -- AUTO_INCREMENT for table `violation_type`
 --
 ALTER TABLE `violation_type`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
